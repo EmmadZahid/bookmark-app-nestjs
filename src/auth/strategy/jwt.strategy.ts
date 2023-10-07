fbb365a60cbea811ddde43b9f7ac40918f4677e3
Emmad <emmad@omnius.com>
2023-10-07 10:06:27 +0500

    Added a get user decorator created a guard Putting user object in request

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,15 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
+import { User } from "@prisma/client";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy){
-    constructor(private config:ConfigService){
+export class JwtStrategy extends PassportStrategy(Strategy,'jwt'){
+    constructor(config:ConfigService, private prisma:PrismaService){
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
             secretOrKey: config.get('JWT_SECERET'),
         })
+    }
+
+    async validate(payload:{sub: number, email: string}): Promise<User>{
+        const user = await this.prisma.user.findUnique({
+            where:{
+                id: payload.sub
+            }
+        })
+        delete user.hash
+
+        return user      //This adds the payload to all the request object
     }
 }
