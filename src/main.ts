0b727e2b46c2082a8958a41eabd079b0f279426a
Emmad <emmad@omnius.com>
2023-10-07 18:39:47 +0500

    configured e2e testing

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app:INestApplication = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true //Trims any extra property sent from client
   }))
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import {Test, TestingModule} from '@nestjs/testing'
+import { AppModule } from '../src/app.module';
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app:INestApplication
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    app = moduleRef.createNestApplication();
+
+    //We have to add pipe here in tests, the same way we add in main.ts
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true //Trims any extra property sent from client in request object
+    }))
+
+    app.init()
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  //tear down
+  afterAll(() => {
+    app.close()
+  })
+
+  it.todo('shoud work')
 });
