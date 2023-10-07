5de5b2e1e474b555ecea99d48dc43057168cb53b
Emmad <emmad@omnius.com>
2023-10-07 07:04:37 +0500

    Added config module to replace using the db configs directly. Now reading from .env (dotenv lib)

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,8 +1,10 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
 
 @Global() //So that we dont have to import in every module
 @Module({
+  imports: [ConfigModule],
   providers: [PrismaService],
   exports: [PrismaService]
 })
