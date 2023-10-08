2355bca769a0ac316fdda4b2b1d9226ca8707beb
Emmad <emmad@omnius.com>
2023-10-08 14:59:31 +0500

    wrote some e2e tests for signup and signin

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,13 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import {Test, TestingModule} from '@nestjs/testing'
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
+import * as pactum from 'pactum';
+import { AuthDto } from 'src/auth/dto/auth.dto';
+
 describe('AppController (e2e)', () => {
   let app:INestApplication
   let prisma:PrismaService
+  let port:number = 3333
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -19,9 +23,11 @@ describe('AppController (e2e)', () => {
     }))
 
     app.init()
-
+    app.listen(port)
     prisma = app.get(PrismaService)
     await prisma.cleanDb()
+
+    pactum.request.setBaseUrl(`http://localhost:${port}`)
   });
 
   //tear down
@@ -29,5 +35,74 @@ describe('AppController (e2e)', () => {
     app.close()
   })
 
-  it.todo('shoud work2')
+  describe('Auth', ()=>{
+    let dto:AuthDto = new AuthDto()
+    dto.email = "emmad@gmail.com"
+    dto.password = "123"
+
+    describe('Signup', ()=>{
+      it('should signup', () =>{
+        return pactum.spec().post('/auth/signup').withBody(dto).expectStatus(201)
+      })
+
+      it('should throw if email is empty', () =>{
+        return pactum.spec().post('/auth/signup').withBody({
+          email: dto.email
+        }).expectStatus(400)
+      })
+
+      it('should throw with no email', () =>{
+        return pactum.spec().post('/auth/signup').withBody({
+          password: dto.password
+        }).expectStatus(400)
+      })
+
+      it('should throw with invalid email', () =>{
+        return pactum.spec().post('/auth/signup').withBody({
+          ...dto, email: "wrongemail@"
+        }).expectStatus(400)
+      })
+    })
+
+
+    describe('Signin', ()=>{
+      it('should signin', () =>{
+        return pactum.spec().post('/auth/signin').withBody(dto).expectStatus(200)
+      })
+
+      it('should throw if email is empty', () =>{
+        return pactum.spec().post('/auth/signin').withBody({
+          email: dto.email
+        }).expectStatus(400)
+      })
+
+      it('should throw with no email', () =>{
+        return pactum.spec().post('/auth/signin').withBody({
+          password: dto.password
+        }).expectStatus(400)
+      })
+
+      it('should throw with invalid email', () =>{
+        return pactum.spec().post('/auth/signin').withBody({
+          ...dto, email: "wrongemail@"
+        }).expectStatus(400)
+      })
+
+      it('should throw with wrong password', () =>{
+        return pactum.spec().post('/auth/signin').withBody({
+          ...dto, password: "wrong pass"
+        }).expectStatus(400)
+      })
+
+      it('should throw with wrong email', () =>{
+        return pactum.spec().post('/auth/signin').withBody({
+          ...dto, email: "wrong email"
+        }).expectStatus(400)
+      })
+    })
+  })
+
+  describe('User', ()=>{})
+
+  describe('Bookmark', ()=>{})
 });
